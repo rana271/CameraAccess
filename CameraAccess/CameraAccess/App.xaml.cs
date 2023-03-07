ceffa7c7fd8c3e1257c5b6392e3f501472863e9f
rana271 <ranacomp@gmail.com>
2023-03-07 11:03:05 +0530

    Add project files.

---
+++ CameraAccess/CameraAccess/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CameraAccess
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            Plugin.Media.CrossMedia.Current.Initialize();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
